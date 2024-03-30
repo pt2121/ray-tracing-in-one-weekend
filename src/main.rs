2e64a7bd97ea38d519a65bb5e2d26041cc307794
Prat T <pt2121@users.noreply.github.com>
2024-03-30 12:03:51 -0700

    modeling a hollow glass sphere (11.5)

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,12 @@ fn main() {
 
     let material_ground = Lambertian::new(color(0.8, 0.8, 0.0));
     let material_center = Lambertian::new(color(0.1, 0.2, 0.5));
-    let material_left = Dielectric::new(1.5);
     let material_right = Metal::new(color(0.8, 0.6, 0.2), 0.0);
     let world_objects: Vec<Box<dyn Hittable>> = vec![
         Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)),
         Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_center)),
-        Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)),
+        Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, Dielectric::new(1.5))),
+        Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.4, Dielectric::new(1.5))),
         Box::new(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, material_right)),
     ];
 
