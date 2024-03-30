82b82dcfe290c328d70fb390f998f49e63d89857
Prat T <pt2121@users.noreply.github.com>
2024-03-30 11:21:59 -0700

    total internal reflection (11.3)

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ fn main() {
     env_logger::init();
 
     let material_ground = Lambertian::new(color(0.8, 0.8, 0.0));
-    let material_center = Dielectric::new(1.5);
+    let material_center = Lambertian::new(color(0.1, 0.2, 0.5));
     let material_left = Dielectric::new(1.5);
-    let material_right = Metal::new(color(0.8, 0.6, 0.2), 1.0);
+    let material_right = Metal::new(color(0.8, 0.6, 0.2), 0.0);
     let world_objects: Vec<Box<dyn Hittable>> = vec![
         Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)),
         Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_center)),
--- src/material.rs
+++ src/material.rs
@@ -102,9 +102,18 @@ impl Material for Dielectric {
             self.index_of_refraction
         };
         let unit_direction = ray.unit_dir();
-        let refracted = refract(unit_direction, hit_record.normal, refraction_ratio);
+        let cos_theta = -unit_direction.dot(hit_record.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract {
+            // Must Reflect
+            reflect(unit_direction, hit_record.normal)
+        } else {
+            // Can Refract
+            refract(unit_direction, hit_record.normal, refraction_ratio)
+        };
 
-        let scattered = Ray::new(hit_record.p, refracted);
+        let scattered = Ray::new(hit_record.p, direction);
         let attenuation = color(1.0, 1.0, 1.0);
         return Some((scattered, attenuation));
     }
